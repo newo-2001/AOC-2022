6d24e5ba634e8125a6af06662a26d74e2b241ab7
newo-2001 <o.elderbroek@gmail.com>
2022-12-01 17:58:20 +0100

    Day 1

---
+++ Day 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Part 1
+var computeCalories = (string rations) => rations
+    .Split("\n")
+    .Select(int.Parse)
+    .Sum();
+
+var calories = File.ReadAllText("rations.txt")
+    .Replace("\r\n", "\n")
+    .Split("\n\n")
+    .Select(computeCalories)
+    .Order();
+
+Console.WriteLine($"The Elf carrying the most calories is carrying {calories.Last()} calories");
+
+// Part 2
+var total = calories.TakeLast(3).Sum();
+Console.WriteLine($"The 3 Elfs carrying the most calories are carrying {total} total calories");
