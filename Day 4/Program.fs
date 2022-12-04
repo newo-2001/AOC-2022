81feefedd22c2e51aaa0c896e1b0ceb0c7a6ad51
newo-2001 <o.elderbroek@gmail.com>
2022-12-04 15:02:37 +0100

    Day 4

---
+++ Day 4/Program.fs
@@ -0,0 +1,37 @@
+﻿open System.IO
+open System
+
+type Group = Range * Range
+
+let sepBy (sep: string) (mapper: string * string -> 'b) (str: string): 'b =
+    match str.Split(sep) with
+        | [| first; second |] -> mapper(first, second)
+        | _ -> failwith("Parsing failed")
+
+let parseRange = sepBy "-" (fun (x, y) -> Range(int x, int y))
+let parseGroup = sepBy "," (fun (x, y) -> Group(parseRange x, parseRange y))
+
+let overlapsCompletely (a: Range, b: Range) =
+    (a.Start.Value >= b.Start.Value && a.End.Value <= b.End.Value) ||
+        (b.Start.Value >= a.Start.Value && b.End.Value <= a.End.Value)
+
+let overlapsPartly (a: Range, b: Range) =
+    (a.End.Value >= b.Start.Value && a.Start.Value <= b.End.Value) ||
+        (b.End.Value >= a.Start.Value && b.Start.Value <= a.End.Value)
+
+let assignments =
+    File.ReadLines "assignments.txt"
+    |> Seq.map parseGroup
+
+let countGroups filter =
+    assignments
+    |> Seq.filter filter
+    |> Seq.length
+
+// Part 1
+let completelyOverlapping = countGroups overlapsCompletely
+printf($"There are {completelyOverlapping} groups that completely overlap\n")
+
+// Part 2
+let partlyOverlapping = countGroups overlapsPartly
+printf($"There are {partlyOverlapping} groups that partly overlap\n")
