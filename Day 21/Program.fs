93bc0f0d001f799626e5542c67a403af3a5461c6
newo-2001 <o.elderbroek@gmail.com>
2022-12-22 21:54:46 +0100

    Day 21

---
+++ Day 21/Program.fs
@@ -0,0 +1,73 @@
+﻿open System
+open System.IO
+open FParsec
+open System.Collections.Generic
+open Tree
+
+type Operation = Add | Sub | Div | Mul
+type ComputationMonkey = {
+    operation: Operation
+    operands: string * string
+}
+
+type Monkey =
+    | Constant of int64
+    | Computation of ComputationMonkey
+
+let parseName = parray 4 anyChar |>> Array.map string |>> Array.reduce(+)
+let parseScalarMonkey =
+    tuple2 (parseName .>> pstring ": ") (pint64 |>> Constant)
+
+let parseOperator = anyChar >>= function
+    | '+' -> preturn Add
+    | '-' -> preturn Sub
+    | '*' -> preturn Mul
+    | '/' -> preturn Div
+    | c -> fail $"Invalid operator: {c}"
+
+let parseComputationMonkey = 
+    let space = pchar ' '
+    pipe4 (parseName .>> pstring ": ") parseName
+        (space >>. parseOperator .>> space) parseName
+        (fun name left operator right ->
+            name, Computation { operation = operator; operands = (left, right) })
+
+let parseMonkey = attempt parseScalarMonkey <|> parseComputationMonkey
+
+let rec buildTree variableSelector (nodes: IDictionary<string, Monkey>) =
+    let buildTree = function
+    | name when variableSelector name -> Variable
+    | name -> buildTree variableSelector nodes nodes[name]
+
+    function
+    | Constant(n) -> Number(n)
+    | Computation(computation) ->
+        let makeOperator operator operands =
+            OpBinary { operator = operator; operands = operands }
+
+        let left = buildTree (fst computation.operands)
+        let right = buildTree (snd computation.operands)
+
+        match computation.operation with
+        | Add -> makeOperator Plus (left, right)
+        | Sub -> makeOperator Minus (left, right)
+        | Mul -> makeOperator Times (left, right)
+        | Div -> makeOperator Over (left, right)
+        
+let monkeys =
+    File.ReadAllText "monkeys.txt"
+    |> run (sepBy parseMonkey skipNewline)
+    |> function
+    | Failure(err, _, _) -> raise (new Exception $"Parsing failed: {err}")
+    | Success(result, _, _) -> result
+    |> List.toSeq
+    |> dict
+
+buildTree (fun _ -> false) monkeys monkeys["root"]
+|> ExpressionTree.evaluate <| 0L
+|> printf "The number the monkey will yell is %i\n"
+
+let f = buildTree (fun name -> name = "humn") monkeys monkeys["root"]
+
+ExpressionTree.solve f
+|> printf "The number you need to yell is %i\n"
---
+++ Day 21/Tree.fs
@@ -0,0 +1,71 @@
+﻿module Tree
+
+type Operator = Plus | Minus | Times | Over
+
+type BinaryOperator = {
+    operator: Operator
+    operands: ExpressionTree * ExpressionTree
+}
+
+and ExpressionTree =
+    | Number of int64
+    | Variable
+    | OpBinary of BinaryOperator
+
+    static member evaluate f x =
+        let rec eval = function
+        | Number(n) -> n
+        | Variable -> x
+        | OpBinary(operation) ->
+            let left, right = operation.operands
+            match operation.operator with
+            | Plus -> eval left + eval right
+            | Minus -> eval left - eval right
+            | Times -> eval left * eval right
+            | Over -> eval left / eval right
+        eval f
+
+    static member isConstant f =
+        let rec isConstant = function
+        | Variable -> false
+        | Number(_) -> true
+        | OpBinary(operation) ->
+            let left, right = operation.operands
+            isConstant left && isConstant right
+        isConstant f
+
+    static member solve =
+        let rec solve f n =
+            match f with
+            | Number(_) -> failwith "How did we get here?"
+            | Variable -> n
+            | OpBinary(operation) ->
+                let left, right = operation.operands
+                if ExpressionTree.isConstant left then
+                    let leftValue = ExpressionTree.evaluate left 0L
+                    match operation.operator with
+                    | Plus -> n - leftValue
+                    | Minus -> leftValue - n
+                    | Times -> n / leftValue
+                    | Over -> leftValue / n
+                    |> solve right
+                else
+                    let rightValue = ExpressionTree.evaluate right 0L
+                    match operation.operator with
+                    | Plus -> n - rightValue
+                    | Minus -> n + rightValue
+                    | Times -> n / rightValue
+                    | Over -> n * rightValue
+                    |> solve left
+        
+        function
+        | OpBinary(operation) ->
+            let left, right = operation.operands
+            let dependant, independant =
+                if ExpressionTree.isConstant left then right, left else left, right
+
+            ExpressionTree.evaluate independant 0L
+            |> solve dependant
+        | _ -> failwith "Root node of expression is not an operator!"
+
+
